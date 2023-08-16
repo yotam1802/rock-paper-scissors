f5bc004420027765e4e09b3fbf2809e28991730a
yotam1802 <Yotam1802@gmail.com>
2023-08-16 16:52:37 -0600

    Add fixSensitivity() and playRound() methods
    fixSensitivity() adjusts for possible case sensitivity when the user responds with rock, papper, or scissors.
    playRound() plays one round of rock paper scissors and returns the result of the round.

--- main.js
+++ main.js
@@ -14,4 +14,32 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function fixCaseSensitivity(playerSelection) {
+    return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+    switch(playerSelection) {
+        case "Rock":
+            if (computerSelection == "Rock") return "It's a tie! Rock and rock.";
+            else if (computerSelection == "Paper") return "You lose! Paper beats rock.";
+            else if (computerSelection == "Scissors") return "You win! Rock beats scissors.";
+            break;
+        case "Paper":
+            if (computerSelection == "Rock") return "You win! Paper beats rock.";
+            else if (computerSelection == "Paper") return "It's a tie! Paper and paper.";
+            else if (computerSelection == "Scissors") return "You lose! Scissors beats paper.";
+            break;
+        case "Scissors":
+            if (computerSelection == "Rock") return "You lose! Rock beats scissors.";
+            else if (computerSelection == "Paper") return "You win! Scissors beats paper.";
+            else if (computerSelection == "Scissors") return "It's a tie! Scissors and scissors.";
+    }
+}
+
+let computerSelection = getComputerChoice();
+let playerSelection = prompt("Please choose Rock, Paper, or Scissors");
+playerSelection = fixCaseSensitivity(playerSelection);
+let result = playRound(playerSelection, computerSelection);
+console.log(result);
+
